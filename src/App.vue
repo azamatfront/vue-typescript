7368e272c2425a2205bf67d0ecc402b16a8b1c9a
azamats-github <azamatstechblog@gmail.com>
2021-11-14 04:35:53 +0500

    Vue Components using TypeScript

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="app">
-    <p>{{ name }}</p>
+    <p>{{ name }} - {{ age }}</p>
+		<button @click="changeName('Developer')">Change name</button>
+		<button @click="changeAge(27)">Change age</button>
   </div>
 </template>
 
 <script lang="ts">
+
+// you can't write inside data() like this instead write (as) keyword inside data()
+let age: number | string = 26
+
 import { defineComponent } from "vue";
 
 export default defineComponent({
@@ -12,13 +18,17 @@ export default defineComponent({
   components: {},
   data() {
     return {
-      name: "Link",
+      name: "Azamat",
+			age: 26 as number | string
     };
   },
   methods: {
     changeName(name: string) {
       this.name = name;
     },
+		changeAge(age: number) {
+			this.age = age
+		}
   },
 });
 </script>
