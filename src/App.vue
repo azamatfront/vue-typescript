8b354e301eb01507cda77cf8bdc472f9ce6d8b24
azamats-github <azamatstechblog@gmail.com>
2021-11-14 04:10:42 +0500

    Settings .eslintrc.js

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="app">
-		Hello World!
-	</div>
+    <p>{{ name }}</p>
+    <a href="/" target="_blank">Home</a>
+    <a href="/" target="_blank">Home</a>
+    <a href="/" target="_blank">Home</a>
+    <a href="/" target="_blank">Home</a>
+    <a href="/" target="_blank">Home</a>
+    <a href="/" target="_blank">Home</a>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'App',
-  components: {}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      name: "Link",
+    };
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name;
+    },
+  },
 });
 </script>
 
-<style>
-
-</style>
+<style></style>
